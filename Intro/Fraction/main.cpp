ce1146ec18eb7301af39e821e375170176171fdf
Новичковская Ольга <miss4411@yandex.ru>
2021-07-10 21:51:45 +0700

    04072021

--- Intro/Fraction/main.cpp
+++ Intro/Fraction/main.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define INCREMENT_CHECK
+//#define ARIFMETICAL_OPERATORS_CHECK
+//#define COMPAUND_ASSIGNMENTS_CHECK
+//#define OPTIMIZASION_CHECK
+//#define TYPE_CONVERSIONS
+#define TYPE_CONVERSIONS_HOME_WORK
+
+class Fraction;// Class Declaration
+Fraction operator*(Fraction left, Fraction right);
 
 class Fraction
 {
@@ -42,7 +55,7 @@ public:
 		this->denominator = 1;
 		cout << "DefaultConstructor:" << this << endl;
 	}
-	Fraction(int integer)
+	 explicit Fraction(int integer)
 	{
 		this->integer = integer;
 		this->numerator = 0;
@@ -85,7 +98,66 @@ public:
 	cout << "CopyAssignment:\t" << this << endl;
 	return *this;
 	}
+	Fraction& operator*=(const Fraction& other)
+	{
+		/*int a = 2;
+		int b = 3;
+		a *= b;
+		a = a * b;
+		*/
+		return *this = *this * other;
+	}
+	Fraction& MultiplyAndAssign(const Fraction& other)
+	{
+		/*int a = 2;
+		int b = 3;
+		a *= b;
+		a = a * b;
+		*/
+		return *this = *this * other;
+		// Возвращает результат операции по сссылке, поскольку результат сохраняется в this
+		//следовательно this является результатом
+		//this-указатель на объект, для которого вызывается метод
+		//Для того, чтобы  вызвать метод, объект должен быть  уже создан
+		// то есть объект появляется раньше, чем для него вызывается метод
+		// и после того как метод отработал с объектом для которого он вызвался (this)
+		//ничего не происходит, он никуда не девается и не удаляется, так как объекты 
+		//может удалять только деструктор
+		//Поскольку этот объект никуда не девается после отработки метода,мы можем вернуть его по ссылке
+		// (by reference), чтобы не создавать копию этого объекта
+	}
 
+	//Increment/Decrement
+
+	Fraction& operator++()// префикс
+	{
+		integer++;
+		return*this;
+	}
+	Fraction operator++(int)//постфикс
+	{
+		Fraction old = *this;//сохраняем старое значение
+		integer++;
+		return old;// CopyConstructor
+	}
+	Fraction& operator--()// Дикремент
+	{
+		integer--;
+		return *this;
+	}
+	Fraction operator--(int)	//Дикремент
+	{
+		Fraction old = *this;
+		integer--;
+		return old;
+	}
+
+	//Type-cast operators:
+	explicit operator int()
+	{
+		return integer;
+
+	}
 	
 	// Metohds
 	void print()const
@@ -98,7 +170,92 @@ public:
 		cout << endl;
 	}
 
+	Fraction& to_improrer()// умножение дробей
+	{
+		numerator += integer * denominator;
+		integer = 0;
+		return*this;
+	}
+
+	Fraction& to_proper()
+	{
+		integer += numerator / denominator;
+		numerator %= denominator;
+		return*this;
+	}
+	Fraction inverted()// обращает дробь
+	{
+		to_improrer();
+		//int buffer = numerator;
+		//numerator = denominator;
+		//denominator = buffer;
+		//return*this;
+		return Fraction(denominator, numerator);
+	}
 };
+
+
+
+
+Fraction operator+(Fraction left, Fraction right)
+{
+	left.to_improrer();
+	right.to_improrer();
+	//Fraction sum
+	//(
+	//	left.get_numerator() * right.get_denominator() + right.get_numerator() * left.get_denominator(),
+	//left.get_denominator() * right.get_denominator()
+	//);
+		//sum.to_proper();
+		//return sum;
+	return Fraction// временный безымянный объект
+	(
+		left.get_numerator() * right.get_denominator() + right.get_numerator() * left.get_denominator(),
+		left.get_denominator() * right.get_denominator()
+	).to_proper();
+
+}
+
+Fraction operator*(Fraction left, Fraction right)
+{
+	left.to_improrer();
+	right.to_improrer();
+	//Fraction result;
+	//result.set_numerator(left.get_numerator() * right.get_numerator());
+	//result.set_denominator(left.get_denominator() * right.get_denominator());
+	//Fraction result
+	//(
+		//left.get_numerator() * right.get_numerator(),
+		//left.get_denominator() * right.get_denominator()
+	//);
+	//return result;
+	// 
+	// Result- это локальный объект, он объявлен в функции operator*
+	//и существует только в этой функции и по завершении этой функции
+	//result будет удален из памяти, и ессли вернуть ссылку на этот локальный объект, то на месте вызова
+	//мы получим мусор
+	// поэтому возвращаем объект по значению, чтобы скопировать его на место вызова
+	return Fraction// Здесь явно вызываем конструктор, который создает временный безымянный объект с нужным нам результатом
+	(
+		left.get_numerator() * right.get_numerator(),
+		left.get_denominator() * right.get_denominator()
+	);
+}
+
+Fraction  operator/(Fraction left, Fraction right)
+{
+	//left.to_improrer();
+	//right.to_improrer();
+	//Fraction result
+	//(
+	//	left.get_numerator() * right.get_denominator(),
+		//left.get_denominator() * right.get_denominator()
+	//);
+	//return result;
+	Fraction result = left * right.inverted();
+	return result;
+	//return left*right.inverted();
+}
 
 //#define CONSTRUCTORS_CHECK
 void main()
@@ -122,4 +279,103 @@ void main()
 	F.print();
 #endif
 
-};
+#ifdef ARIFMETICAL_OPERATORS_CHECK
+
+
+	int a = 2;
+	int b = 3;
+	int c = a * b;
+	int d = a / b;
+	int e = a + b;
+
+	Fraction A(2, 3, 4);
+	Fraction B(3, 4, 5);
+	Fraction C = A * B;
+	C.print();
+	Fraction D = A / B;
+	D.print();
+
+	Fraction E = A + B;
+	E.print();
+	//A.print();
+	//A.to_improrer();
+	//A.print();
+	//A.to_proper();
+	//A.print();
+
+	(A + B).print();
+
+#endif // ARIFMETICAL_OPERATORS_CHECK
+
+#ifdef INCREMENT_CHECK
+
+	for (double i = .25; i < 10; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+
+	for (Fraction i(1, 4); i.get_integer() < 10; i++)
+	{
+		i.print();
+	}
+	cout << endl;
+#endif
+
+#ifdef COMPAUND_ASSIGNMENTS_CHECK
+
+	Fraction A(2, 3, 4);
+	Fraction B(3, 4, 5);
+	A *= B;// неявный вызов оператора implicit
+	A.operator*=(B);//явный вызов explicit operator call
+	A.MultiplyAndAssign(B);
+	A.print();
+
+	A* B;// неявный	 вызов оператора
+	operator*(A, B);// явный вызов оператора
+#endif
+
+
+
+#ifdef OPTIMISATION_CHECK
+	Fraction A(2, 3, 4);
+	Fraction B(3, 4, 5);
+	Fraction C = A * B;
+	C.print();
+#endif // OPTIMISATION_CHECK
+
+#ifdef TYPE_CONVERSIONS
+	//int a = 2;// no conversion Нет преобразований
+//double b = 3;// from int to double (From less to more)
+//int c = 4.5;// from double to int (from more to less with data loss)
+//int d = b;// from double to int( from more to less no data loss)
+//int e = 5.3;
+//int f = a + e;
+//cout << f;
+
+//OTHER TYPES TO OUR TYPE
+
+	int a = 2;
+	Fraction A = (Fraction)5;// from int to Fraction from less to more
+	A.print();
+	Fraction B;
+	B = (Fraction)3;
+	B.print();
+
+	//OUR OBJECTS TO OTHER TYPES
+	// operator type()
+	//{
+	//}
+	int b = (int)B;
+	cout << b << endl;
+#endif // TYPE_CONVERSIONS
+	// Task 1
+//	Fraction A(2, 3, 4);
+//	double b = A;
+//	cout << a << endl;
+//	//Task 2
+//	double b = 3.14;
+//	Fraction B = b;
+//	B.print();
+//
+//};
